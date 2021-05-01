1a14a03d7fca2e99ca694b64c9eb48f581bff17d
irostub <irostub@gmail.com>
2021-05-01 13:13:13 +0900

    Init commit

---
+++ src/main/java/com/rainbowflavor/hdcweb/HdcWebApplication.java
@@ -0,0 +1,13 @@
+package com.rainbowflavor.hdcweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HdcWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HdcWebApplication.class, args);
+	}
+
+}
