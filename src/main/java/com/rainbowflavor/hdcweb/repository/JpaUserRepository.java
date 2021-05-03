c19044369693027838c77b82125a4af90652d810
irostub <irostub@gmail.com>
2021-05-03 23:52:04 +0900

    Add repository

---
+++ src/main/java/com/rainbowflavor/hdcweb/repository/JpaUserRepository.java
@@ -0,0 +1,11 @@
+package com.rainbowflavor.hdcweb.repository;
+
+import com.rainbowflavor.hdcweb.domain.User;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface JpaUserRepository extends JpaRepository<User, Long>, UserRepository{
+    @Override
+    Optional<User> findByName(String name);
+}
