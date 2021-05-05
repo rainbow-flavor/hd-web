83f807a07744e4703fca48ac459408692c18ee7b
irostub <irostub@gmail.com>
2021-05-05 15:03:55 +0900

    Fix builder pattern

--- src/main/java/com/rainbowflavor/hdcweb/domain/User.java
+++ src/main/java/com/rainbowflavor/hdcweb/domain/User.java
@@ -1,22 +1,18 @@
 package com.rainbowflavor.hdcweb.domain;
 
-import lombok.AllArgsConstructor;
-import lombok.Builder;
-import lombok.Data;
-import lombok.NoArgsConstructor;
+import lombok.*;
 
 import javax.persistence.*;
 import java.util.Date;
 import java.util.HashSet;
+import java.util.List;
 import java.util.Set;
 
 @NoArgsConstructor
-@AllArgsConstructor
-@Builder
 @Entity
 @Data
 public class User {
-    @Id @Column(name="USER_ID")
+    @Id @Column(name="USER_ID") @Setter(AccessLevel.NONE)
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
@@ -46,4 +42,20 @@ public class User {
 
     @OneToMany(mappedBy = "user")
     private Set<UserRole> roles = new HashSet<>();
+
+    @OneToMany
+    @JoinColumn(name = "id")
+    private List<Schedule> schedule;
+
+    @Builder
+    public User(String name, String username, String password, String email, String position, String phone, String address, Date birth) {
+        this.name = name;
+        this.username = username;
+        this.password = password;
+        this.email = email;
+        this.position = position;
+        this.phone = phone;
+        this.address = address;
+        this.birth = birth;
+    }
 }
