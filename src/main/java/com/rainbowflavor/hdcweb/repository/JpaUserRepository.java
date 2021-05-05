f509f8860702560a163ff64e423f447501989609
irostub <irostub@gmail.com>
2021-05-05 15:01:31 +0900

    Remove useless method

--- src/main/java/com/rainbowflavor/hdcweb/repository/JpaUserRepository.java
+++ src/main/java/com/rainbowflavor/hdcweb/repository/JpaUserRepository.java
@@ -5,7 +5,5 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.Optional;
 
-public interface JpaUserRepository extends JpaRepository<User, Long>, UserRepository{
-    @Override
-    Optional<User> findByName(String name);
+public interface JpaUserRepository extends JpaRepository<User, Long>{
 }
