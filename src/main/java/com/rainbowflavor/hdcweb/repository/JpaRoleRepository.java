4c4f0f8415c6019cc9d9a6940fa5b3e091f40df7
irostub <irostub@gmail.com>
2021-05-05 15:48:37 +0900

    Add jpa repository

---
+++ src/main/java/com/rainbowflavor/hdcweb/repository/JpaRoleRepository.java
@@ -0,0 +1,7 @@
+package com.rainbowflavor.hdcweb.repository;
+
+import com.rainbowflavor.hdcweb.domain.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface JpaRoleRepository extends JpaRepository<Role, Long> {
+}
