444670189a06770bd05044e1b8a159bc52fb1fa4
irostub <irostub@gmail.com>
2021-05-05 23:27:45 +0900

    Remove useless package

--- src/main/java/com/rainbowflavor/hdcweb/controller/HomeController.java
+++ src/main/java/com/rainbowflavor/hdcweb/controller/HomeController.java
@@ -4,14 +4,14 @@ import com.rainbowflavor.hdcweb.calendar.DateData;
 import com.rainbowflavor.hdcweb.dto.CalendarDto;
 import com.rainbowflavor.hdcweb.string.ConstantUrl;
 import lombok.extern.slf4j.Slf4j;
-import org.aspectj.bridge.IMessage;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.util.*;
 
 @Slf4j
