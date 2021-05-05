c1cdeedffaefb9d1799293c8398dec1cad366a16
irostub <irostub@gmail.com>
2021-05-05 15:48:22 +0900

    Add auditing time stamp

--- src/main/java/com/rainbowflavor/hdcweb/HdcWebApplication.java
+++ src/main/java/com/rainbowflavor/hdcweb/HdcWebApplication.java
@@ -2,8 +2,10 @@ package com.rainbowflavor.hdcweb;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @SpringBootApplication
+@EnableJpaAuditing
 public class HdcWebApplication {
 
 	public static void main(String[] args) {
