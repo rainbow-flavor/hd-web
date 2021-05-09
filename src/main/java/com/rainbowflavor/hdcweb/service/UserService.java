57abdb01b3754a780f8a31088c6709318808c3cf
irostub <irostub@gmail.com>
2021-05-09 22:23:16 +0900

    Implement UserService
    add joinUser logic

--- src/main/java/com/rainbowflavor/hdcweb/service/UserService.java
+++ src/main/java/com/rainbowflavor/hdcweb/service/UserService.java
@@ -4,6 +4,7 @@ import com.rainbowflavor.hdcweb.domain.ERole;
 import com.rainbowflavor.hdcweb.domain.Role;
 import com.rainbowflavor.hdcweb.domain.User;
 import com.rainbowflavor.hdcweb.domain.UserRole;
+import com.rainbowflavor.hdcweb.dto.SignupDto;
 import com.rainbowflavor.hdcweb.repository.JpaRoleRepository;
 import com.rainbowflavor.hdcweb.repository.JpaUserRepository;
 import com.rainbowflavor.hdcweb.repository.JpaUserRoleRepository;
@@ -11,7 +12,6 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.Optional;
 
 @Transactional
 @RequiredArgsConstructor
@@ -21,13 +21,24 @@ public class UserService {
     private final JpaUserRepository userRepository;
     private final JpaUserRoleRepository userRoleRepository;
 
-    public Long joinUser(User user) {
+    public SignupDto joinUser(SignupDto signupDto) {
         UserRole userRole = new UserRole();
+        User user = User.builder()
+                .name(signupDto.getName())
+                .password(signupDto.getPassword())
+                .birth(signupDto.getBirthday())
+                .phone(signupDto.getPhone())
+                .position(signupDto.getPosition())
+                .email(signupDto.getEmail())
+                .address(signupDto.getAddress())
+                .build();
+
         User saveUser = userRepository.save(user);
         Role findRole = roleRepository.findByRole(ERole.ROLE_ADMIN);
         userRole.setUser(saveUser);
         userRole.setRole(findRole);
         UserRole saveUserRole = userRoleRepository.save(userRole);
-        return saveUserRole.getId();
+
+        return null;
     }
 }
