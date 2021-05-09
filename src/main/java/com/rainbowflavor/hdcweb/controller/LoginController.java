a0a49261e32ca438133a9daceee107c8a6642c33
irostub <irostub@gmail.com>
2021-05-09 19:33:40 +0900

    Fix mapping path and make signup mapping

--- src/main/java/com/rainbowflavor/hdcweb/controller/LoginController.java
+++ src/main/java/com/rainbowflavor/hdcweb/controller/LoginController.java
@@ -1,21 +1,33 @@
 package com.rainbowflavor.hdcweb.controller;
 
+import com.rainbowflavor.hdcweb.dto.SignupDto;
 import com.rainbowflavor.hdcweb.string.ConstantUrl;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
 @Controller
-@RequestMapping(value={ConstantUrl.LOGIN, ConstantUrl.ROOT})
+@RequestMapping(value={ ConstantUrl.ROOT})
 public class LoginController {
 
     @GetMapping
+    public String rootPage(){
+        return "redirect:/signin";
+    }
+
+    @GetMapping(value="/signin")
     public String loginPage(HttpServletRequest request){
         log.info("Login Controller = {}", request);
         return "page/signin";
+    }
+
+    @ResponseBody
+    @PostMapping(value="/signup")
+    public String signUp(@ModelAttribute SignupDto signupDto){
+        log.info("signup obj = {}",signupDto);
+        return "ok";
     }
 }
