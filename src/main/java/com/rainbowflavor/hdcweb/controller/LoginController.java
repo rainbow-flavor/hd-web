01b69737727fda58bb17c5023eb9984b2a0f8b08
irostub <irostub@gmail.com>
2021-05-09 22:22:40 +0900

    Add dependency userService in loginController

--- src/main/java/com/rainbowflavor/hdcweb/controller/LoginController.java
+++ src/main/java/com/rainbowflavor/hdcweb/controller/LoginController.java
@@ -1,7 +1,9 @@
 package com.rainbowflavor.hdcweb.controller;
 
 import com.rainbowflavor.hdcweb.dto.SignupDto;
+import com.rainbowflavor.hdcweb.service.UserService;
 import com.rainbowflavor.hdcweb.string.ConstantUrl;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
@@ -9,9 +11,12 @@ import org.springframework.web.bind.annotation.*;
 import javax.servlet.http.HttpServletRequest;
 
 @Slf4j
-@Controller
+@RequiredArgsConstructor
 @RequestMapping(value={ ConstantUrl.ROOT})
+@Controller
 public class LoginController {
+
+    private final UserService userService;
 
     @GetMapping
     public String rootPage(){
@@ -27,7 +32,8 @@ public class LoginController {
     @ResponseBody
     @PostMapping(value="/signup")
     public String signUp(@ModelAttribute SignupDto signupDto){
-        log.info("signup obj = {}",signupDto);
+        userService.joinUser(signupDto);
         return "ok";
     }
 }
+
