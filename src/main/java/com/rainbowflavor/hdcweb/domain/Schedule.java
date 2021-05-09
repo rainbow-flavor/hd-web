ee9b07e2d1859f06f2113ffb592a559b9f0a3348
diph-Ho <ghrbs00@gmail.com>
2021-05-09 15:49:09 +0900

    fix calendar.html, update Schedule.java and sidebar.html

--- src/main/java/com/rainbowflavor/hdcweb/domain/Schedule.java
+++ src/main/java/com/rainbowflavor/hdcweb/domain/Schedule.java
@@ -3,18 +3,29 @@ package com.rainbowflavor.hdcweb.domain;
 import lombok.*;
 
 import javax.persistence.*;
+import java.util.Date;
 
 @Entity
 @Getter
 @Setter
 @NoArgsConstructor
-public class Schedule extends AuditingTimeStamp{
+public class Schedule extends AuditingTimeStamp {
 
-    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Setter(AccessLevel.NONE)
     private Long id;
 
     @ManyToOne
-    @JoinColumn(name="user_id")
+    @JoinColumn(name = "user_id")
     private User user;
+
+    @Temporal(TemporalType.DATE)
+    private Date scheduleStartDate;
+
+    @Temporal(TemporalType.DATE)
+    private Date scheduleEndDate;
+
+    @Column
+    private String scheduleDetail;
 }
