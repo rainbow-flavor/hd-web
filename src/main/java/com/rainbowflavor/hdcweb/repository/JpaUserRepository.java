c73fe7e33236f43b39dd6f01217896635414cc39
irostub <irostub@enjoyworks.co.kr>
2021-05-11 12:59:23 +0900

    Add select query
    role and user

--- src/main/java/com/rainbowflavor/hdcweb/repository/JpaUserRepository.java
+++ src/main/java/com/rainbowflavor/hdcweb/repository/JpaUserRepository.java
@@ -3,6 +3,12 @@ package com.rainbowflavor.hdcweb.repository;
 import com.rainbowflavor.hdcweb.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 
 public interface JpaUserRepository extends JpaRepository<User, Long>{
+
+    Optional<User> findByName(String name);
+
+    User findByEmail(String email);
 }
