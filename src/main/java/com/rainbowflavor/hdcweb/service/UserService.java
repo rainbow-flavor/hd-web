d0b91624ebaa3fb7d3053aceacd5c5c625deb4e1
irostub <irostub@gmail.com>
2021-05-16 21:20:12 +0900

    Implement UserDetailService

--- src/main/java/com/rainbowflavor/hdcweb/service/UserService.java
+++ src/main/java/com/rainbowflavor/hdcweb/service/UserService.java
@@ -10,19 +10,28 @@ import com.rainbowflavor.hdcweb.repository.JpaRoleRepository;
 import com.rainbowflavor.hdcweb.repository.JpaUserRepository;
 import com.rainbowflavor.hdcweb.repository.JpaUserRoleRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
+import java.util.Optional;
 
+@Slf4j
 @RequiredArgsConstructor
 @Service
-public class UserService {
+public class UserService implements UserDetailsService {
     private final JpaRoleRepository roleRepository;
     private final JpaUserRepository userRepository;
     private final JpaUserRoleRepository userRoleRepository;
+    private final PasswordEncoder passwordEncoder;
 
     @Transactional
     public SignupDto joinUser(SignupDto signupDto) {
+        signupDto.setPassword(passwordEncoder.encode(signupDto.getPassword()));
         User user = SignupMapper.INSTANCE.toEntity(signupDto);
 
         User saveUser = userRepository.save(user);
@@ -41,4 +50,17 @@ public class UserService {
         return userRepository.getOne(userId);
     }
 
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        Optional<User> findUser = userRepository.findByName(username);
+        User user = findUser.orElseThrow(() -> new UsernameNotFoundException(username));
+        log.debug("login user = {}",user);
+
+        return org.springframework.security.core.userdetails.User.builder()
+                .username(user.getName())
+                .password(user.getPassword())
+                .roles("ADMIN")
+                .build();
+    }
 }
