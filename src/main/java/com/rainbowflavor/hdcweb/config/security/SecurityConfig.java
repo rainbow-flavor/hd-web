cad90fbb6abc8f5917a1c62ca802cde9c9ec58d9
irostub <irostub@gmail.com>
2021-05-16 21:19:44 +0900

    Add spring security config

---
+++ src/main/java/com/rainbowflavor/hdcweb/config/security/SecurityConfig.java
@@ -0,0 +1,36 @@
+package com.rainbowflavor.hdcweb.config.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@Configuration
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http
+                .authorizeRequests()
+                .mvcMatchers("/", "/favicon.ico", "/css/**","/js/**", "/webjars/**","/img/**", "/login", "/signup").permitAll()
+                .anyRequest().authenticated()
+                .and()
+                .httpBasic()
+                .and()
+                .formLogin()
+                .loginPage("/login")
+                .permitAll();
+
+        http.logout().logoutSuccessUrl("/");
+    }
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+    }
+}
