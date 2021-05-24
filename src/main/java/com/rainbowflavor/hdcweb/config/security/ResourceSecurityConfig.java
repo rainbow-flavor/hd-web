8b2083abff0678a8be3e5361ddd42e7ad512f632
irostub <irostub@gmail.com>
2021-05-24 21:50:11 +0900

    Add resource security configuration

---
+++ src/main/java/com/rainbowflavor/hdcweb/config/security/ResourceSecurityConfig.java
@@ -0,0 +1,14 @@
+package com.rainbowflavor.hdcweb.config.security;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@Configuration
+public class ResourceSecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        super.configure(http);
+    }
+}
