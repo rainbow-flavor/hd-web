b640e9ad66dd1995b1ca831e3bb629478d14e3c1
= <irostub@gmail.com>
2021-05-28 13:04:45 +0900

    Add ignore authenticate static resource config

--- src/main/java/com/rainbowflavor/hdcweb/config/security/SecurityConfig.java
+++ src/main/java/com/rainbowflavor/hdcweb/config/security/SecurityConfig.java
@@ -1,8 +1,10 @@
 package com.rainbowflavor.hdcweb.config.security;
 
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -28,6 +30,11 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
                 .permitAll();
 
         http.logout().logoutSuccessUrl("/");
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web.ignoring().requestMatchers(PathRequest.toStaticResources().atCommonLocations());
     }
 
     @Bean
